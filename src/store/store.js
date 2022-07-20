2d18007413db3a14bb61524f8d1a91cae9ab7baa
AlexJamisone <business.homeit@gmail.com>
2022-07-20 23:12:47 +0300

    add persisore and store cart in localStore

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,16 @@
 import { compose, applyMiddleware, createStore} from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
 import { rootReducer } from './root-reducer'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) 
 
 const middlewars = [logger]
 
@@ -9,7 +19,9 @@ const composeEnhancers = compose(applyMiddleware(
 ))
 
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     undefined,
     composeEnhancers
 )
+
+export const persistor = persistStore(store)
