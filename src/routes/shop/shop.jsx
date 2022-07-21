508513690293c87c0cd3640931d64ae19cca7ace
AlexJamisone <business.homeit@gmail.com>
2022-07-21 16:06:04 +0300

    debug

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import { getCategoriesAndDocument } from '../../utils/firebase/firebase.utils'
 import { fetchCategoriesAsync } from '../../store/categories/categories.action'
 import { Routes, Route } from 'react-router-dom'
 import CategorisPreview from '../categorys-preview/categoris-preview'
