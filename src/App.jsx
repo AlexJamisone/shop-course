43be4ff705d454ce69ebb35b562642e33c71db4b
AlexJamisone <business.homeit@gmail.com>
2022-07-24 18:05:47 +0300

    add current user in firebase/start migration firebase in saga

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import { 
 	onAuthStateChangedListener,
 	createUserDocumentFromAuth,
+	getCurrentUser
  } from "./utils/firebase/firebase.utils";
 import Navigation from "./routes/navigation/navigation";
 import Home from './routes/home/home';
@@ -18,13 +19,9 @@ const App = () => {
 	const dispatch = useDispatch()
 
 	useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if(user) {
-                createUserDocumentFromAuth(user)
-            }
-            dispatch(setCurrentUser(user))
-        })
-        return unsubscribe
+		getCurrentUser()
+			.then(user => console.log(user))
+			.catch((err) => console.log(err));
     }, [])
 
  
