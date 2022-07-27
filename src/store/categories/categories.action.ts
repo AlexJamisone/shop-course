53655c84923f43e27dffe5515e81760d5f5cb273
AlexJamisone <business.homeit@gmail.com>
2022-07-27 19:35:55 +0300

    categories selector to typescript

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,27 @@
+import { CATEGORIES_ACTION, Category } from "./categories.types";
+import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils";
+
+
+export type FetchCategoriesStart = 
+    Action<CATEGORIES_ACTION.FETCH_CATEGORIES_START>
+export type FetchCategoriesSuccess = 
+    ActionWithPayload<CATEGORIES_ACTION.FETCH_CATEGORIES_SUCCESS, Category[]>
+export type FetchCategoriesFaild = 
+    ActionWithPayload<CATEGORIES_ACTION.FETCH_CATEGORIES_FAILED, Error>
+
+
+export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFaild
+
+
+export const fetchCategoriesStart = (): FetchCategoriesStart => 
+    createAction(CATEGORIES_ACTION.FETCH_CATEGORIES_START)
+
+export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess => 
+    createAction(
+        CATEGORIES_ACTION.FETCH_CATEGORIES_SUCCESS,
+        categoriesArray
+    )
+    
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFaild => 
+    createAction(CATEGORIES_ACTION.FETCH_CATEGORIES_FAILED, error)
+
